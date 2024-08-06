a3fa4e9fe99e2a49f193480af3fe0b799d8be18b
pjs0719 <pjssoj07193@gmail.com>
2024-08-06 18:30:57 +0900

    메인 페이지 디자인 수정

--- lib/mainListPage.dart
+++ lib/mainListPage.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'dart:math';
-
-import 'sensorService.dart';
+import 'package:spaghetti/sensorService.dart';
 
 class MainListPage extends StatefulWidget {
   const MainListPage({super.key});
@@ -36,7 +34,7 @@ class _MyWidgetState extends State<MainListPage> {
       final screenWidth = mediaQuery.size.width;
 
       return Scaffold(
-        resizeToAvoidBottomInset: false, // 키보드 오버플로우 방지
+        resizeToAvoidBottomInset: false,
         body: PageView(
           children: [
             Container(
@@ -72,111 +70,105 @@ class _MyWidgetState extends State<MainListPage> {
                         style: TextStyle(fontSize: screenWidth * 0.05)),
                   ),
                   Positioned(
-                    top: screenHeight * 0.23 + 30, // "이전 수업" 텍스트 아래 30px
+                    top: screenHeight * 0.28,
                     left: screenWidth * 0.1,
-                    child: SizedBox(
-                        width: screenWidth * 0.8,
-                        height: screenHeight * 0.45, // 목록을 위한 높이 조정
-                        child: GridView.builder(
-                          controller: _scrollController, // ScrollController 추가
-                          padding: EdgeInsets.zero, // GridView의 패딩을 없앰
-                          itemCount: senserList.length,
-                          gridDelegate:
-                              SliverGridDelegateWithFixedCrossAxisCount(
-                            crossAxisCount: 2, // 2열로 설정
-                            childAspectRatio: (screenWidth / 2) /
-                                (screenHeight * 0.15), // 항목의 가로 세로 비율 설정
-                            crossAxisSpacing: 10, // 열 간의 간격
-                            mainAxisSpacing: 50, // 행 간의 간격
-                          ),
-                          itemBuilder: (context, index) {
-                            SensorData sensor = senserList[index];
-                            return Column(
-                              children: [
-                                Padding(
-                                  padding:
-                                      const EdgeInsets.symmetric(vertical: 8.0),
-                                  child: Row(
-                                    children: [
-                                      // 작은 아이콘 추가
-                                      SizedBox(width: 10),
-                                      Expanded(
-                                        child: Column(
-                                          crossAxisAlignment:
-                                              CrossAxisAlignment.start,
-                                          children: [
-                                            Container(
-                                              width: screenWidth * 0.3,
-                                              height: screenHeight * 0.15,
-                                              decoration: BoxDecoration(
-                                                color: Colors.white,
-                                                borderRadius:
-                                                    BorderRadius.circular(12),
-                                                boxShadow: [
-                                                  BoxShadow(
-                                                    color: Colors.black
-                                                        .withOpacity(0.1),
-                                                    blurRadius: 10,
-                                                    offset: Offset(0, 5),
-                                                  ),
-                                                ],
-                                              ),
-                                              child: Center(
-                                                child: Text(sensor.name),
-                                              ),
-                                            )
-                                          ],
-                                        ),
-                                      ),
-                                    ],
-                                  ),
-                                ),
-                              ],
-                            );
-                          },
-                        )),
-                  ),
-
-                  Positioned(
-                    left: screenWidth * 0.1,
-                    bottom: screenHeight * 0.1 - 50, // 하단에서 50px 위로
                     child: SizedBox(
                       width: screenWidth * 0.8,
-                      height: screenHeight * 0.06, // 화면 너비의 80%
-                      child: ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                          backgroundColor: Color(0xff789ad0),
-                          surfaceTintColor: Color(0xff789ad0),
-                          foregroundColor: Colors.white,
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(12),
-                          ),
+                      height: screenHeight * 0.45,
+                      child: GridView.builder(
+                        controller: _scrollController,
+                        padding: EdgeInsets.zero,
+                        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                          crossAxisCount: 2,
+                          childAspectRatio: 1.0,
+                          crossAxisSpacing: 10.0,
+                          mainAxisSpacing: 10.0,
                         ),
-                        onPressed: () {},
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Text(
-                              "수업 생성하기",
-                              style: TextStyle(color: Colors.white),
+                        itemCount: senserList.length,
+                        itemBuilder: (context, index) {
+                          SensorData sensor = senserList[index];
+                          return GestureDetector(
+                            onTap: () {
+                              showDialog(
+                                context: context,
+                                builder: (BuildContext context) {
+                                  return AlertDialog(
+                                    shape: RoundedRectangleBorder(
+                                      borderRadius:
+                                          BorderRadius.all(Radius.circular(20)),
+                                    ),
+                                    contentPadding: EdgeInsets.all(20),
+                                    content: Column(
+                                      mainAxisSize: MainAxisSize.min,
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: [
+                                        Text(
+                                          sensor.name,
+                                          style: TextStyle(
+                                              fontSize: 18,
+                                              fontWeight: FontWeight.bold),
+                                        ),
+                                        SizedBox(height: 10),
+                                        Text(sensor.date),
+                                        SizedBox(height: 10),
+                                        Text(sensor.detailText),
+                                      ],
+                                    ),
+                                    actions: [
+                                      TextButton(
+                                        onPressed: () {
+                                          Navigator.of(context).pop();
+                                        },
+                                        child: Text('닫기'),
+                                      ),
+                                    ],
+                                  );
+                                },
+                              );
+                            },
+                            child: Card(
+                              elevation: 2,
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(12),
+                              ),
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Icon(Icons.sensor_window),
+                                  SizedBox(height: 8),
+                                  Text(
+                                    sensor.name,
+                                    overflow: TextOverflow.ellipsis,
+                                    style: TextStyle(
+                                      fontSize: 16.0,
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.black,
+                                      fontFamily: 'NanumB',
+                                    ),
+                                  ),
+                                  SizedBox(height: 4),
+                                  Text(
+                                    sensor.date,
+                                    style: TextStyle(
+                                      fontSize: 12.0,
+                                      color: Colors.grey,
+                                    ),
+                                  ),
+                                ],
+                              ),
                             ),
-                            Text(
-                              "+",
-                              style: TextStyle(color: Colors.white),
-                            ),
-                          ],
-                        ),
+                          );
+                        },
                       ),
                     ),
                   ),
-
-                  // logout 아이콘의 Positioned 위치
                   Positioned(
                     right: screenWidth * 0.1,
                     top: screenHeight * 0.12,
                     child: IconButton(
                       icon: Image.asset(
-                        'assets/images/logout.png', // 이미지 경로 확인
+                        'assets/images/logout.png',
                         width: screenWidth * 0.08,
                         height: screenWidth * 0.08,
                       ),
@@ -208,8 +200,7 @@ class _MyWidgetState extends State<MainListPage> {
                                       Expanded(
                                         child: ElevatedButton(
                                           onPressed: () {
-                                            Navigator.of(context)
-                                                .pop(); // 모달 닫기
+                                            Navigator.of(context).pop();
                                           },
                                           style: ElevatedButton.styleFrom(
                                             backgroundColor: Color(0xfff7f8fc),
@@ -224,7 +215,7 @@ class _MyWidgetState extends State<MainListPage> {
                                                   color: Color(0xff789bd0))),
                                         ),
                                       ),
-                                      SizedBox(width: 10), // 버튼 사이 간격
+                                      SizedBox(width: 10),
                                       Expanded(
                                         child: ElevatedButton(
                                           onPressed: () {},
