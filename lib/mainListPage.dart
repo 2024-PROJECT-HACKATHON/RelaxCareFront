16e7fd5246bd6a011498269b924909b501b36736
pbs <pbs1057@hs.ac.kr>
2024-08-06 22:17:37 +0900

    Row Column 으로 변경

--- lib/mainListPage.dart
+++ lib/mainListPage.dart
@@ -34,220 +34,71 @@ class _MyWidgetState extends State<MainListPage> {
       final screenWidth = mediaQuery.size.width;
 
       return Scaffold(
-        resizeToAvoidBottomInset: false,
-        body: PageView(
-          children: [
-            Container(
-              child: Stack(
-                children: [
-                  Positioned(
-                    left: screenWidth * 0.13,
-                    top: screenHeight * 0.11,
-                    child: Image.asset(
-                      'assets/images/profil1.png',
-                      width: screenWidth * 0.15,
-                      height: screenWidth * 0.15,
-                    ),
-                  ),
-                  Positioned(
-                    left: screenWidth * 0.31,
-                    top: screenHeight * 0.12,
-                    child: Text('서울집',
-                        style: TextStyle(fontSize: screenWidth * 0.04)),
-                  ),
-                  Positioned(
-                    left: screenWidth * 0.31,
-                    top: screenHeight * 0.15,
-                    child: Text('님',
-                        style: TextStyle(
-                            fontSize: screenWidth * 0.03,
-                            color: Color(0xff424141))),
-                  ),
-                  Positioned(
-                    left: screenWidth * 0.12,
-                    top: screenHeight * 0.23,
-                    child: Text('오늘도 건강하게 하루를 보내고 계십니다',
-                        style: TextStyle(fontSize: screenWidth * 0.05)),
-                  ),
-                  Positioned(
-                    top: screenHeight * 0.28,
-                    left: screenWidth * 0.1,
-                    child: SizedBox(
-                      width: screenWidth * 0.8,
-                      height: screenHeight * 0.45,
-                      child: GridView.builder(
-                        controller: _scrollController,
-                        padding: EdgeInsets.zero,
-                        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                          crossAxisCount: 2,
-                          childAspectRatio: 1.0,
-                          crossAxisSpacing: 10.0,
-                          mainAxisSpacing: 10.0,
-                        ),
-                        itemCount: senserList.length,
-                        itemBuilder: (context, index) {
-                          SensorData sensor = senserList[index];
-                          return GestureDetector(
-                            onTap: () {
-                              showDialog(
-                                context: context,
-                                builder: (BuildContext context) {
-                                  return AlertDialog(
-                                    shape: RoundedRectangleBorder(
-                                      borderRadius:
-                                          BorderRadius.all(Radius.circular(20)),
-                                    ),
-                                    contentPadding: EdgeInsets.all(20),
-                                    content: Column(
-                                      mainAxisSize: MainAxisSize.min,
-                                      crossAxisAlignment:
-                                          CrossAxisAlignment.start,
-                                      children: [
-                                        Text(
-                                          sensor.name,
-                                          style: TextStyle(
-                                              fontSize: 18,
-                                              fontWeight: FontWeight.bold),
-                                        ),
-                                        SizedBox(height: 10),
-                                        Text(sensor.date),
-                                        SizedBox(height: 10),
-                                        Text(sensor.detailText),
-                                      ],
-                                    ),
-                                    actions: [
-                                      TextButton(
-                                        onPressed: () {
-                                          Navigator.of(context).pop();
-                                        },
-                                        child: Text('닫기'),
-                                      ),
-                                    ],
-                                  );
-                                },
-                              );
-                            },
-                            child: Card(
-                              elevation: 2,
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(12),
-                              ),
-                              child: Column(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: [
-                                  Icon(Icons.sensor_window),
-                                  SizedBox(height: 8),
-                                  Text(
-                                    sensor.name,
-                                    overflow: TextOverflow.ellipsis,
-                                    style: TextStyle(
-                                      fontSize: 16.0,
-                                      fontWeight: FontWeight.bold,
-                                      color: Colors.black,
-                                      fontFamily: 'NanumB',
-                                    ),
-                                  ),
-                                  SizedBox(height: 4),
-                                  Text(
-                                    sensor.date,
-                                    style: TextStyle(
-                                      fontSize: 12.0,
-                                      color: Colors.grey,
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            ),
-                          );
-                        },
-                      ),
-                    ),
-                  ),
-                  Positioned(
-                    right: screenWidth * 0.1,
-                    top: screenHeight * 0.12,
-                    child: IconButton(
-                      icon: Image.asset(
-                        'assets/images/logout.png',
-                        width: screenWidth * 0.08,
-                        height: screenWidth * 0.08,
-                      ),
-                      iconSize: screenWidth * 0.08,
-                      onPressed: () {
-                        showDialog(
-                          context: context,
-                          builder: (BuildContext context) {
-                            return AlertDialog(
-                              shape: RoundedRectangleBorder(
-                                borderRadius:
-                                    BorderRadius.all(Radius.circular(20)),
-                              ),
-                              contentPadding: EdgeInsets.all(20),
-                              content: Column(
-                                mainAxisSize: MainAxisSize.min,
-                                children: [
-                                  Text(
-                                    '로그아웃 하시겠습니까?',
-                                    style: TextStyle(
-                                        fontSize: 18,
-                                        fontWeight: FontWeight.bold),
-                                  ),
-                                  SizedBox(height: 20),
-                                  Row(
-                                    mainAxisAlignment:
-                                        MainAxisAlignment.spaceEvenly,
-                                    children: [
-                                      Expanded(
-                                        child: ElevatedButton(
-                                          onPressed: () {
-                                            Navigator.of(context).pop();
-                                          },
-                                          style: ElevatedButton.styleFrom(
-                                            backgroundColor: Color(0xfff7f8fc),
-                                            shape: RoundedRectangleBorder(
-                                              borderRadius:
-                                                  BorderRadius.circular(8),
-                                            ),
-                                          ),
-                                          child: Text('취소',
-                                              style: TextStyle(
-                                                  fontFamily: "NanumEB",
-                                                  color: Color(0xff789bd0))),
-                                        ),
-                                      ),
-                                      SizedBox(width: 10),
-                                      Expanded(
-                                        child: ElevatedButton(
-                                          onPressed: () {},
-                                          style: ElevatedButton.styleFrom(
-                                            backgroundColor: Color(0xff789bd0),
-                                            shape: RoundedRectangleBorder(
-                                              borderRadius:
-                                                  BorderRadius.circular(8),
-                                            ),
-                                          ),
-                                          child: Text('로그아웃',
-                                              style: TextStyle(
-                                                  fontFamily: "NanumEB",
-                                                  color: Colors.white)),
-                                        ),
-                                      ),
-                                    ],
-                                  ),
-                                ],
-                              ),
-                            );
-                          },
-                        );
-                      },
-                    ),
-                  ),
-                ],
-              ),
+          body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          SizedBox(
+            height: screenHeight * 0.075,
+          ),
+          Row(children: [
+            SizedBox(
+              width: screenWidth * 0.075,
             ),
-          ],
-        ),
-      );
+            Text('서울집', style: TextStyle(fontSize: screenWidth * 0.07)),
+            SizedBox(
+              width: screenWidth * 0.4,
+            ),
+            Image.asset(
+              'assets/images/signal.png',
+              width: screenWidth * 0.1,
+              height: screenWidth * 0.1,
+            ),
+            SizedBox(
+              width: screenWidth * 0.05,
+            ),
+            Image.asset(
+              'assets/images/profil1.png',
+              width: screenWidth * 0.1,
+              height: screenWidth * 0.1,
+            ),
+          ]),
+          SizedBox(
+            height: screenHeight * 0.04,
+          ),
+          Row(
+            children: [
+              SizedBox(
+                width: screenWidth * 0.075,
+              ),
+              Text('오늘도 건강하게 하루를 \n보내고 계십니다',
+                  style: TextStyle(fontSize: screenWidth * 0.04)),
+              SizedBox(
+                width: screenWidth * 0.3,
+              ),
+              Image.asset(
+                'assets/images/check.png',
+                width: screenWidth * 0.1,
+                height: screenWidth * 0.1,
+              ),
+            ],
+          ),
+          SizedBox(
+            height: screenHeight * 0.01,
+          ),
+          Row(
+            children: [
+              SizedBox(
+                width: screenWidth * 0.075,
+              ),
+              Container(
+                height: 3,
+                width: screenWidth * 0.8,
+                color: Colors.black,
+              ),
+            ],
+          )
+        ],
+      ));
     });
   }
 }
