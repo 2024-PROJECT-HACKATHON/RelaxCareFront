fc36b126003204d91e3eb919a6d0012f7f0c1a92
pbs <pbs1057@hs.ac.kr>
2024-08-06 23:24:43 +0900

    MainListPage 수정

--- lib/main.dart
+++ lib/main.dart
@@ -18,6 +18,23 @@ class MyApp extends StatelessWidget {
       ],
       child: MaterialApp(
         home: MainListPage(),
+        theme: ThemeData(
+          fontFamily: 'NanumB',
+          scaffoldBackgroundColor: Colors.white, // 전체 앱의 Scaffold 배경색을 흰색으로 설정
+          dialogBackgroundColor: Colors.white, // 다이얼로그 배경색을 흰색으로 설정
+          appBarTheme: AppBarTheme(
+            backgroundColor: Colors.white, // AppBar 배경색을 흰색으로 설정
+            iconTheme:
+                IconThemeData(color: Colors.black), // AppBar 아이콘 색상을 검정으로 설정
+            titleTextStyle: TextStyle(
+              color: Colors.black, // AppBar 타이틀 텍스트 색상을 검정으로 설정
+              fontSize: 20.0,
+              fontWeight: FontWeight.bold,
+              fontFamily: 'NanumB',
+            ),
+            elevation: 0, // AppBar 그림자 없애기
+          ),
+        ),
       ),
     );
   }
--- lib/mainListPage.dart
+++ lib/mainListPage.dart
@@ -34,71 +34,161 @@ class _MyWidgetState extends State<MainListPage> {
       final screenWidth = mediaQuery.size.width;
 
       return Scaffold(
-          body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          SizedBox(
-            height: screenHeight * 0.075,
-          ),
-          Row(children: [
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
             SizedBox(
-              width: screenWidth * 0.075,
+              height: screenHeight * 0.075,
             ),
-            Text('서울집', style: TextStyle(fontSize: screenWidth * 0.07)),
+            Row(
+              children: [
+                SizedBox(
+                  width: screenWidth * 0.075,
+                ),
+                Text('서울집', style: TextStyle(fontSize: screenWidth * 0.07)),
+                SizedBox(
+                  width: screenWidth * 0.4,
+                ),
+                Image.asset(
+                  'assets/images/signal.png',
+                  width: screenWidth * 0.1,
+                  height: screenWidth * 0.1,
+                ),
+                SizedBox(
+                  width: screenWidth * 0.05,
+                ),
+                Image.asset(
+                  'assets/images/profil1.png',
+                  width: screenWidth * 0.1,
+                  height: screenWidth * 0.1,
+                ),
+              ],
+            ),
             SizedBox(
-              width: screenWidth * 0.4,
+              height: screenHeight * 0.04,
             ),
-            Image.asset(
-              'assets/images/signal.png',
-              width: screenWidth * 0.1,
-              height: screenWidth * 0.1,
+            Row(
+              children: [
+                SizedBox(
+                  width: screenWidth * 0.075,
+                ),
+                Text(
+                  '오늘도 건강하게 하루를 \n보내고 계십니다',
+                  style: TextStyle(fontSize: screenWidth * 0.04),
+                ),
+                SizedBox(
+                  width: screenWidth * 0.3,
+                ),
+                Image.asset(
+                  'assets/images/check.png',
+                  width: screenWidth * 0.1,
+                  height: screenWidth * 0.1,
+                ),
+              ],
             ),
             SizedBox(
-              width: screenWidth * 0.05,
+              height: screenHeight * 0.01,
             ),
-            Image.asset(
-              'assets/images/profil1.png',
-              width: screenWidth * 0.1,
-              height: screenWidth * 0.1,
+            Row(
+              children: [
+                SizedBox(
+                  width: screenWidth * 0.075,
+                ),
+                Container(
+                  height: 3,
+                  width: screenWidth * 0.85,
+                  color: Colors.black,
+                ),
+              ],
             ),
-          ]),
-          SizedBox(
-            height: screenHeight * 0.04,
-          ),
-          Row(
-            children: [
-              SizedBox(
-                width: screenWidth * 0.075,
+            SizedBox(
+              height: screenHeight * 0.01,
+            ),
+            Expanded(
+              child: Padding(
+                padding: EdgeInsets.symmetric(horizontal: screenWidth * 0.075),
+                child: GridView.builder(
+                  controller: _scrollController,
+                  padding: EdgeInsets.zero,
+                  itemCount: senserList.length,
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    childAspectRatio: (screenWidth / 3) / (screenHeight * 0.15),
+                    crossAxisSpacing: 20,
+                    mainAxisSpacing: 20,
+                  ),
+                  itemBuilder: (context, index) {
+                    SensorData sensor = senserList[index];
+                    return Container(
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(12),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black.withOpacity(0.1),
+                            blurRadius: 10,
+                            offset: Offset(0, 5),
+                          ),
+                        ],
+                      ),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          SizedBox(
+                            height: screenWidth * 0.025,
+                          ),
+                          Row(
+                            children: [
+                              SizedBox(
+                                width: screenWidth * 0.025,
+                              ),
+                              Image.asset(
+                                sensor.sensorImage,
+                                width: screenWidth * 0.1,
+                                height: screenWidth * 0.1,
+                              ),
+                              SizedBox(
+                                width: screenWidth * 0.15,
+                              ),
+                              Text(sensor.name)
+                            ],
+                          ),
+                          SizedBox(
+                            height: screenWidth * 0.05,
+                          ),
+                          Row(
+                            children: [
+                              SizedBox(
+                                width: screenWidth * 0.025,
+                              ),
+                              Expanded(
+                                child: Text(sensor.date,
+                                    style: TextStyle(color: Colors.grey)),
+                              )
+                            ],
+                          ),
+                          Row(
+                            children: [
+                              SizedBox(
+                                width: screenWidth * 0.025,
+                              ),
+                              Expanded(
+                                child: Text(
+                                  sensor.detailText,
+                                ),
+                              )
+                            ],
+                          )
+                        ],
+                      ),
+                    );
+                  },
+                ),
               ),
-              Text('오늘도 건강하게 하루를 \n보내고 계십니다',
-                  style: TextStyle(fontSize: screenWidth * 0.04)),
-              SizedBox(
-                width: screenWidth * 0.3,
-              ),
-              Image.asset(
-                'assets/images/check.png',
-                width: screenWidth * 0.1,
-                height: screenWidth * 0.1,
-              ),
-            ],
-          ),
-          SizedBox(
-            height: screenHeight * 0.01,
-          ),
-          Row(
-            children: [
-              SizedBox(
-                width: screenWidth * 0.075,
-              ),
-              Container(
-                height: 3,
-                width: screenWidth * 0.8,
-                color: Colors.black,
-              ),
-            ],
-          )
-        ],
-      ));
+            ),
+          ],
+        ),
+      );
     });
   }
 }
